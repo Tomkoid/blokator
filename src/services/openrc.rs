5bfa58d9cb0be36ff6c47a574f99515e9584d218
Tomkoid <tomkoid@tomkoid.tk>
2022-09-08 17:09:30 +0200

    Added license to all files

--- src/services/openrc.rs
+++ src/services/openrc.rs
@@ -1,3 +1,22 @@
+// openrc.rs
+//
+// Simple cross-platform and system-wide CLI adblocker
+// Copyright (C) 2022 Tomáš Zierl
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+
 use std::process::Command;
 
 pub fn networkmanager_openrc_restart() -> Result<std::process::ExitStatus, std::io::Error> {
--- src/sync.rs
+++ src/sync.rs
@@ -1,3 +1,21 @@
+// sync.rs
+//
+// Simple cross-platform and system-wide CLI adblocker
+// Copyright (C) 2022 Tomáš Zierl
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
 use std::{process::exit, path::Path};
 
 use crate::{write::write_to_file, get_data_dir, read::read_file_to_string, initialize_colors::initialize_colors};
