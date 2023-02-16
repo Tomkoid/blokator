e7f1b0e65d73868500ac4c82c42efc56ceff1bc5
Tomkoid <tomkoid@tomkoid.tk>
2023-02-16 17:52:38 +0100

    Removed licensing from source files

--- src/services/mod.rs
+++ src/services/mod.rs
@@ -1,21 +1,3 @@
-// services/mod.rs
-//
-// Simple cross-platform and system-wide CLI adblocker
-// Copyright (C) 2023 Tomáš Zierl
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program. If not, see <https://www.gnu.org/licenses/>.
-
 pub mod init;
 pub mod networkmanager;
 pub mod openrc;
